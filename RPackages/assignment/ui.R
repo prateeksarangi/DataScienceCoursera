79712a105f930b8ba93cff8cd6386f26ee26a7c4
prateeksarangi <sarangiprateek80@gmail.com>
2020-04-11 18:17:48 +0530

    Cleaning UI

--- RPackages/assignment/ui.R
+++ RPackages/assignment/ui.R
@@ -27,37 +27,20 @@ shinyUI(
                                                 tabPanel("Regression model", 
                                                          plotOutput("mpgPlot"),
                                                          verbatimTextOutput("fit")
-                                                         
                                                 )
-                                                
                                     )
-                                    
                                 )
-                                
                             )
-                            
                         )
-                        
                ),
                
                tabPanel("About the Data Set",
-                        
-                        
-                        
                         h3("Regression Models Course Project (from Coursera)"),
-                        
                         helpText("You work for Motor Trend, a magazine about the automobile industry Looking at a data set of a collection of cars, they are interested in exploring the relationship",
-                                 
                                  "between a set of variables and miles per gallon (MPG) (outcome). They are particularly interested in the following two questions: Is an automatic or manual transmission better for MPG. Quantify the MPG difference between automatic and manual transmissions"),
-                        
                         h3("Important"),
-                        
                         p("A data frame with 32 observations on 11 variables."),
-                        
-                        
-                        
                         a("https://class.coursera.org/regmods-008")
-                        
                ),
                
                tabPanel("More Data Detail",
